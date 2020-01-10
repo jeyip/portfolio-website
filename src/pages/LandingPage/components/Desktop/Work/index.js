1cd81c60650a6b3ca4f56156cc49d68c87eb8f24
Jeremy Yip <jeremy.yip7@gmail.com>
2020-01-10 01:45:47 -0800

    Add more work to portfolio

--- src/pages/LandingPage/components/Desktop/Work/index.js
+++ src/pages/LandingPage/components/Desktop/Work/index.js
@@ -9,9 +9,15 @@ import styles from "./work.module.css"
 const workSamples = [
   {
     thumbnailUrl:
+      "https://res.cloudinary.com/jjcodepen/image/upload/c_fit,h_400/v1578649238/Portfolio2_doixla.gif",
+    originUrl: "https://codepen.io/jeyip/pen/povpoBK",
+    id: 1,
+  },
+  {
+    thumbnailUrl:
       "https://res.cloudinary.com/jjcodepen/image/upload/v1578381710/Portfolio1_tt52te.gif",
     originUrl: "https://codepen.io/jeyip/pen/yLyPRpa",
-    id: 1,
+    id: 2,
   },
 ]
 
@@ -25,11 +31,13 @@ const WorkView = () => {
         style={{
           display: "flex",
           flexWrap: "wrap",
-          justifyContent: "center",
+          justifyContent: "flex-start",
         }}
       >
         {workSamples.map(({ id, thumbnailUrl, originUrl }) => (
-          <Tile originUrl={originUrl} thumbnailUrl={thumbnailUrl} key={id} />
+          <div key={id} style={{ marginBottom: "20px", marginRight: "30px" }}>
+            <Tile originUrl={originUrl} thumbnailUrl={thumbnailUrl} key={id} />
+          </div>
         ))}
       </div>
     </div>
