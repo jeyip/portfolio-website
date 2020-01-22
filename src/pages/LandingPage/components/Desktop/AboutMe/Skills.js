b8ced601f05ec00da927015ef5985eb233c71d87
Jeremy Yip <jeremy.yip7@gmail.com>
2020-01-21 17:29:01 -0800

    Refactor AboutMe styling

---
+++ src/pages/LandingPage/components/Desktop/AboutMe/Skills.js
@@ -0,0 +1,54 @@
+import React from "react"
+
+import Header from "../../../shared/SectionHeader"
+import Paragraph from "../../../shared/Paragraph"
+
+import styles from "./skills.module.css"
+
+const SKILLS_TEXT = `I believe deeply in the value of user-centric design.
+Technology is a tool, and delight is important, but the first priority of any
+website or web app should be creating the most intuitive experience possible.`
+
+const skillData = [
+  {
+    title: "Coding",
+    imageUrl: "@/images/codeIcon.svg",
+  },
+  {
+    title: "Story-Mapping",
+    imageUrl: "@/images/javascriptIcon.svg",
+  },
+  {
+    title: "UI/UX",
+    imageUrl: "@/images/javascriptIcon.svg",
+  },
+]
+
+const SkillBox = ({ data }) => (
+  <div className={styles.skillBoxContainer}>
+    <img
+      alt="javascriptIcon"
+      clssName={styles.skillBoxImage}
+      src={require("@/images/codeIcon.svg")}
+    />
+    <h3>{data.title}</h3>
+  </div>
+)
+
+const Skills = () => {
+  return (
+    <div className={styles.skillsContainer}>
+      <div className={styles.headerContainer}>
+        <Header text="MY SKILLS" />
+      </div>
+      <div style={styles.skillBoxesContainer}>
+        {skillData.map(data => (
+          <SkillBox data={data} key={`${data.title}-${data.imageUrl}`} />
+        ))}
+      </div>
+      <Paragraph text={SKILLS_TEXT} />
+    </div>
+  )
+}
+
+export default Skills
--- src/pages/LandingPage/components/Desktop/AboutMe/index.js
+++ src/pages/LandingPage/components/Desktop/AboutMe/index.js
@@ -1,29 +1,25 @@
-import React, { Component } from "react"
+import React from "react"
 
 import { addDesktopLayout } from "../../../../../utils/addLayout"
 import Header from "../../../shared/SectionHeader"
 import Paragraph from "../../../shared/Paragraph"
-import SkillsSection from "./SkillsSection"
+import Skills from "./Skills"
 
-import "./AboutMe.css"
+import styles from "./aboutMe.module.css"
 
-const aboutMeText = `I'm an American front-end software engineer based in the beautiful beachside Ventura, CA. When not at work, you'll find me staying out way, way too late salsa dancing,
+const ABOUT_ME_TEXT = `I'm an American front-end software engineer based in the beautiful beachside Ventura, CA. When not at work, you'll find me staying out way, way too late salsa dancing,
 working towards competitive numbers in powerlifting, and zipping around on my cruiser.`
 
-class AboutMe extends Component {
-  render() {
-    return (
-      <div className="aboutMe" style={{ display: "flex" }}>
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <div style={{ marginBottom: "30px" }}>
-            <Header text="ABOUT ME" />
-          </div>
-          <Paragraph text={aboutMeText} style={{ width: "50%" }} />
-          <SkillsSection />
-        </div>
+const AboutMe = () => {
+  return (
+    <div className={styles.aboutMeContainer}>
+      <div className={styles.headerContainer}>
+        <Header text="ABOUT ME" />
       </div>
-    )
-  }
+      <Paragraph text={ABOUT_ME_TEXT} style={styles.paragraph} />
+      <Skills />
+    </div>
+  )
 }
 
 export default addDesktopLayout(AboutMe)
